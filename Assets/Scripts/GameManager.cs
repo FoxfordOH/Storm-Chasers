2710cdcdf680ff355dd8513d37a8bb8e93a42e6d
ScottFero <ferosp@miamioh.edu>
2025-04-17 19:27:19 -0400

    Player + UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,14 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField] private GameObject player = null;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // player.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
