d6071e4446d024536a385176bdb4ba9eba4b7dcf
FoxfordOH <lomnicnj@miamioh.edu>
2025-04-17 19:28:22 -0400

    GunTime

---
+++ Assets/Scripts/GunManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GunManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector2 playerPos = Camera.main.WorldToViewportPoint (transform.position);
+		Vector2 mousePos = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+		float angle = PointToward(playerPos, mousePos);
+		transform.rotation =  Quaternion.Euler (new Vector3(0f,0f,angle));
+	}
+
+	float PointToward(Vector3 a, Vector3 b) {
+		return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+	}
+}
+
