8c39c2a16443d3d92ed77d58840b0a710c8b4c01
FoxfordOH <lomnicnj@miamioh.edu>
2025-04-19 13:44:20 -0400

    Revert "Merge pull request #5 from FoxfordOH/branch_scottworking"
    This reverts commit 12dfbf439d777a6d43943c4aefc13360b6bf45e5, reversing
    changes made to c5900d694c02ba7cc32c29b9facdebf85bffe9b3.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,11 @@ public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private GameObject player = null;
+    [SerializeField] private GameObject StartMenu = null;   
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameStart();
+    StartMenu.SetActive(true);
     }
 
     // Update is called once per frame
@@ -20,6 +21,7 @@ public class GameManager : MonoBehaviour
     public void GameStart()
     {
     player.transform.position = new Vector3(0.0f, 0.0f, transform.position.z);
+    StartMenu.SetActive(false);
     }    
     
     public void GameQuit()
